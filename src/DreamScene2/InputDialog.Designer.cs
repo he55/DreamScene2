411eeb093d242c2e6f48484e9db22ee6e0352f1e
he55 <40764272+he55@users.noreply.github.com>
2021-12-30 08:52:55 +0800

    重命名控件名称
    Co-Authored-By: Ding only <16121723+dinggeonly@users.noreply.github.com>

--- src/DreamScene2/InputDialog.Designer.cs
+++ src/DreamScene2/InputDialog.Designer.cs
@@ -30,9 +30,9 @@ namespace DreamScene2
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.lblTipMsg = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -44,41 +44,45 @@ namespace DreamScene2
             this.label1.TabIndex = 0;
             this.label1.Text = "请输入 URL:";
             // 
-            // textBox1
+            // txtUrl
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 21);
-            this.textBox1.TabIndex = 1;
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(12, 36);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(259, 21);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://";
             // 
-            // button1
+            // btnConfirm
             // 
-            this.button1.Location = new System.Drawing.Point(196, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "确定";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnConfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfirm.Location = new System.Drawing.Point(196, 68);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "确定";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
-            // label2
+            // lblTipMsg
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(12, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.lblTipMsg.AutoSize = true;
+            this.lblTipMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblTipMsg.Location = new System.Drawing.Point(12, 73);
+            this.lblTipMsg.Name = "lblTipMsg";
+            this.lblTipMsg.Size = new System.Drawing.Size(59, 12);
+            this.lblTipMsg.TabIndex = 3;
+            this.lblTipMsg.Text = "提示信息:";
             // 
             // InputDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(280, 99);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTipMsg);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtUrl);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -95,8 +99,8 @@ namespace DreamScene2
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label lblTipMsg;
     }
 }
