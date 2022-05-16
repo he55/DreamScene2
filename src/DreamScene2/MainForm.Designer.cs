6163eaffac14e1e915b77e4e85cc499139ba6eb3
he55 <40764272+he55@users.noreply.github.com>
2022-05-16 22:15:03 +0800

    Remove show desktop

--- src/DreamScene2/MainForm.Designer.cs
+++ src/DreamScene2/MainForm.Designer.cs
@@ -65,7 +65,6 @@ namespace DreamScene2
             this.toolStripMenuItem16 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem17 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem13 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem28 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem14 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
@@ -169,13 +168,12 @@ namespace DreamScene2
             this.toolStripSeparator1,
             this.toolStripMenuItem16,
             this.toolStripMenuItem13,
-            this.toolStripMenuItem28,
             this.toolStripMenuItem1,
             this.toolStripMenuItem14,
             this.toolStripSeparator3,
             this.toolStripMenuItem4});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 424);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 402);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // toolStripMenuItem15
@@ -381,13 +379,6 @@ namespace DreamScene2
             this.toolStripMenuItem13.Text = "隐藏桌面图标";
             this.toolStripMenuItem13.Click += new System.EventHandler(this.toolStripMenuItem13_Click);
             // 
-            // toolStripMenuItem28
-            // 
-            this.toolStripMenuItem28.Name = "toolStripMenuItem28";
-            this.toolStripMenuItem28.Size = new System.Drawing.Size(168, 22);
-            this.toolStripMenuItem28.Text = "显示桌面";
-            this.toolStripMenuItem28.Click += new System.EventHandler(this.toolStripMenuItem28_Click);
-            // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
@@ -499,7 +490,6 @@ namespace DreamScene2
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem26;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem13;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem27;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem28;
     }
 }
 
