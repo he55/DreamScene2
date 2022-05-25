15395d447ffdb238b1c5aed1da3a50e9a2e49bf5
he55 <40764272+he55@users.noreply.github.com>
2022-05-25 22:10:23 +0800

    Delete old icon

--- src/DreamScene2/Properties/Resources.Designer.cs
+++ src/DreamScene2/Properties/Resources.Designer.cs
@@ -79,25 +79,5 @@ namespace DreamScene2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon icon {
-            get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
