82713ca2633d40cd000077dbee101ad4c6fe03c8
he55 <40764272+he55@users.noreply.github.com>
2022-07-31 21:57:47 +0800

    Create RecentFile.cs

---
+++ src/DreamScene2/RecentFile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DreamScene2
+{
+    public static class RecentFile
+    {
+        static readonly string s_recentPath = Helper.GetPathForAppFolder("recent.txt");
+        static readonly List<string> s_recentFiles = new List<string>();
+
+        public static List<string> Load()
+        {
+            if (File.Exists(s_recentPath))
+            {
+                string[] paths = File.ReadAllLines(s_recentPath);
+                s_recentFiles.AddRange(paths);
+            }
+            return s_recentFiles;
+        }
+
+        public static void Save()
+        {
+            File.WriteAllLines(s_recentPath, s_recentFiles);
+        }
+
+        public static void Clean()
+        {
+            s_recentFiles.Clear();
+            File.WriteAllText(s_recentPath, "");
+        }
+
+        public static void Update(string path)
+        {
+            if (s_recentFiles.Count == 0 || s_recentFiles[0] != path)
+            {
+                if (s_recentFiles.Contains(path))
+                    s_recentFiles.Remove(path);
+
+                s_recentFiles.Insert(0, path);
+            }
+        }
+    }
+}
