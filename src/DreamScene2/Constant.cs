0f071f11cec4ddf183d510c6d819d6c3ec41827d
he55 <40764272+he55@users.noreply.github.com>
2023-12-25 21:28:30 +0800

    update version

--- src/DreamScene2/Constant.cs
+++ src/DreamScene2/Constant.cs
@@ -3,7 +3,7 @@
     public static class Constant
     {
         public const string ProjectName = "DreamScene2";
-        public const string Version = "1.6";
+        public const string Version = "1.7";
         public const string MainWindowTitle = ProjectName + " (～￣▽￣)～";
     }
 }
